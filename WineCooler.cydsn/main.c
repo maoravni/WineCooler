84873eec8df1bb4282ca38d951d3c32054ba8fa1
maoravni <maor.avni@gmail.com>
2015-12-15 22:55:33 +0200

    Support for storing the setpoint in the EEPROM

--- WineCooler.cydsn/main.c
+++ WineCooler.cydsn/main.c
@@ -15,63 +15,58 @@
 #include "rtd.h"
 #include "display.h"
 
-//#define TEMP_MIN -273.0
-//#define TEMP_MAX 2500
-//
-//void DisplayTemp(float value, uint8 row, uint8 col, int dummy)
-//{
-//    char displayStr[20];
-//	uint8 tempCntr, stringEndflag = 0 ;
-//    //volatile float valueTemp;
-//
-//	/* If the value is within the measurable temperature range 
-//	 * then convert the value to string and display it */
-//	if( ( value > TEMP_MIN ) && ( value < TEMP_MAX ) )
-//	{
-//		/* Convert float to string for LCD display using sprintf(stdio.h)*/
-//        //valueTemp = value;
-//	    sprintf(displayStr, "%f", (float)value);
-//		for(tempCntr = 0 ; tempCntr < 16; tempCntr++)
-//		{
-//			if((displayStr[tempCntr] == '\0') && (stringEndflag == 0))
-//			{
-//				stringEndflag = 1;
-//				/* 0xDF prints degree sign */
-//				displayStr[tempCntr++] = 0xDF;
-//				displayStr[tempCntr++] = 'C';
-//			}
-//			if(stringEndflag)
-//			{
-//				displayStr[tempCntr] = ' ';
-//			}
-//		}
-//		displayStr[16] = '\0';
-//	
-//	    LCD_Position(row, col);
-//	    LCD_PrintString(displayStr);
-//
-//	}
-//	else
-//	{
-//	    LCD_Position(row, col);
-//	    LCD_PrintString((char8 *)"Connection Err ");
-//		
-//	}
-//}
-
 int secCounter = 0;
+
+float g_setpoint;
+
+#define M_MIN_SETPOINT 10
+#define M_MAX_SETPOINT 20
+
+cystatus WriteSetpointToEeprom(float value)
+{
+    char* valueArray = (void*)&value;
+    cystatus result;
+    
+    result = EEPROM_1_WriteByte(valueArray[0], 0);
+    if (result != CYRET_SUCCESS)
+        return result;
+    EEPROM_1_WriteByte(valueArray[1], 1);
+    if (result != CYRET_SUCCESS)
+        return result;
+    EEPROM_1_WriteByte(valueArray[2], 2);
+    if (result != CYRET_SUCCESS)
+        return result;
+    EEPROM_1_WriteByte(valueArray[3], 3);
+
+    return result;
+}
+
+float ReadSetpointFromEeprom()
+{
+    char valueArray[4];
+    float *result;
+    
+    valueArray[0] = EEPROM_1_ReadByte(0);
+    valueArray[1] = EEPROM_1_ReadByte(1);
+    valueArray[2] = EEPROM_1_ReadByte(2);
+    valueArray[3] = EEPROM_1_ReadByte(3);
+
+    result = (float*)valueArray;
+    
+    return *result;
+}
 
 void InitializeComponents(void)
 {
     /* Start LCD and set position and print start message */
     LCD_Start();
-    LCD_Position(0, 0);
-    LCD_PrintString("  Temperature   ");
-    LCD_Position(1, 0);
-    LCD_PrintString("  Sensor Kit   ");
+//    LCD_Position(0, 0);
+//    LCD_PrintString("  Temperature   ");
+//    LCD_Position(1, 0);
+//    LCD_PrintString("  Sensor Kit   ");
 	
 	/* Display for 1 second */
-    CyDelay(1000);
+//    CyDelay(1000);
     
     /* Start capsense and initialize baselines and enable scanning of menu buttons */
 //    CapSense_Start();
@@ -106,6 +101,15 @@ void InitializeComponents(void)
 
     TimerInterrupt_Start();
     
+    EEPROM_1_Start();
+    
+    g_setpoint = ReadSetpointFromEeprom();
+    
+    if (g_setpoint < M_MIN_SETPOINT || g_setpoint > M_MAX_SETPOINT)
+    {
+        g_setpoint = 16;
+        WriteSetpointToEeprom(g_setpoint);
+    }
 }
 
 #define M_STATE_SLEEP 0
@@ -149,23 +153,33 @@ int main()
         switch (monitorState)
         {
             case M_STATE_SLEEP:
-            LCD_PrintString("Sleep");
-            if (secCounter >= 6)
-            monitorState = M_STATE_MONITOR;
-            break;
-            case M_STATE_MONITOR:
-            LCD_PrintString("Off");
-            if (temperature > 17)
-                monitorState = M_STATE_COOLING;
-            break;
-            case M_STATE_COOLING:
-            LCD_PrintString("On");
-            if (temperature <= 16)
             {
-                monitorState = M_STATE_SLEEP;
-                secCounter = 0;
+                LCD_PrintString("Sleep");
+                if (secCounter >= 6)
+                {
+                    monitorState = M_STATE_MONITOR;
+                }
+                break;
             }
-            break;
+            case M_STATE_MONITOR:
+            {
+                LCD_PrintString("Off");
+                if (temperature > (g_setpoint + 1))
+                {
+                    monitorState = M_STATE_COOLING;
+                }
+                break;
+            }
+            case M_STATE_COOLING:
+            {
+                LCD_PrintString("On");
+                if (temperature <= g_setpoint)
+                {
+                    monitorState = M_STATE_SLEEP;
+                    secCounter = 0;
+                }
+                break;
+            }
         }
                 
                 
